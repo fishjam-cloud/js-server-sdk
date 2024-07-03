00908b5010a69a9bbcfa5b967c584999c05aeb29
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-07-03 13:20:53 +0200

    remove token from openapi sh

--- packages/fishjam-openapi/openapi.sh
+++ packages/fishjam-openapi/openapi.sh
@@ -11,11 +11,14 @@ ROOTDIR=$(dirname "$(readlink -f $0)")
 if [ -z "$1" ]; then
   echo "Missing a tag or a branch name as the first argument" 1>&2
   exit 1
+elif [ -z "$2" ]; then
+  echo "Missing github token as as second argument" 1>&2
+  exit 1
 fi
 echo "Generating code for $1...\n"
 
 cd $ROOTDIR \
 && npx @openapitools/openapi-generator-cli generate \
-  -i https://raw.githubusercontent.com/fishjam-cloud/fishjam/main/openapi.yaml?token=GHSAT0AAAAAACKKREWRUWGIG5M7QZMTOSX2ZUFEU2A \
+  -i https://raw.githubusercontent.com/fishjam-cloud/fishjam/$1/openapi.yaml?token=$2 \
   -g typescript-axios \
   -o ./src/generated
