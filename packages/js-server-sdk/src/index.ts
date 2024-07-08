fff83443931cdc2db4be4fa92ad1d65a02cbe472
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-07-08 11:55:23 +0200

    update the env names to match the ones used in gh workflow

--- packages/js-server-sdk/src/index.ts
+++ packages/js-server-sdk/src/index.ts
@@ -1,4 +1,4 @@
 export { Peer, PeerStatus, RoomConfig, PeerOptions } from 'fishjam-openapi';
 export { FishjamClient } from './client';
 export * from './exceptions';
-export * from './types';
+export type * from './types';
