b57aed7649f9d7caee4557a893d9afed43bdfcd4
Karol Konkol <56369082+Karolk99@users.noreply.github.com>
2024-07-15 17:34:08 +0200

    Add websocket notifications (#7)
    * Add websocket notifications
    
    * Add allowedNotification type
    
    * Change requests
    
    * Change requests
    
    * Requested changes

--- packages/js-server-sdk/src/proto.ts
+++ packages/js-server-sdk/src/proto.ts
@@ -1,1 +1,1 @@
-export * from 'fishjam-proto';
+export { ServerMessage, ServerMessage_AuthRequest, ServerMessage_EventType } from 'fishjam-proto';
--- packages/js-server-sdk/src/types.ts
+++ packages/js-server-sdk/src/types.ts
@@ -1,7 +1,13 @@
 import { Peer, RoomConfig } from 'fishjam-openapi';
+import { ServerMessage } from './proto';
 
 export type Room = {
   id: string;
   peers: Peer[];
   config: RoomConfig;
+};
+
+export type FishjamConfig = {
+  fishjamUrl: string;
+  serverToken: string;
 };
