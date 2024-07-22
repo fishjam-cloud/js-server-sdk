f62a0f84b60bf49e197c3f0b5eb38d81192e19e7
Piotr Wodecki <44680063+PiotrWodecki@users.noreply.github.com>
2024-07-22 11:06:40 +0200

    Fix Fishjam name (#13)

--- examples/room-manager/src/utils.ts
+++ examples/room-manager/src/utils.ts
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 type RoomManagerError = {
   error:
-    | 'Cannot connect to the FishJam instance'
-    | 'Cannot authorize with the FishJam instance'
-    | 'Invalid request structure sent to the FishJam instance'
-    | 'Unknown error when connecting to the FishJam instance'
-    | "Room doesn't exist"
-    | 'Peer limit has been reached'
-    | 'Internal server error';
+  | 'Cannot connect to the Fishjam instance'
+  | 'Cannot authorize with the Fishjam instance'
+  | 'Invalid request structure sent to the Fishjam instance'
+  | 'Unknown error when connecting to the Fishjam instance'
+  | "Room doesn't exist"
+  | 'Peer limit has been reached'
+  | 'Internal server error';
   cause?: unknown;
   path?: string;
   method?: string;
@@ -25,18 +25,18 @@ export function parseError(error: unknown): [RoomManagerError, number] {
     const statusCode = error.status ?? error.response?.status;
 
     if (!statusCode) {
-      errorMessage = 'Cannot connect to the FishJam instance';
+      errorMessage = 'Cannot connect to the Fishjam instance';
     } else if (statusCode === 404) {
       errorMessage = "Room doesn't exist";
     } else if (statusCode === 401) {
-      errorMessage = 'Cannot authorize with the FishJam instance';
+      errorMessage = 'Cannot authorize with the Fishjam instance';
     } else if (statusCode === 400) {
-      errorMessage = 'Invalid request structure sent to the FishJam instance';
+      errorMessage = 'Invalid request structure sent to the Fishjam instance';
     } else if (statusCode === 503) {
       errorMessage = 'Peer limit has been reached';
       errorCode = 410;
     } else {
-      errorMessage = 'Unknown error when connecting to the FishJam instance';
+      errorMessage = 'Unknown error when connecting to the Fishjam instance';
     }
 
     parsedError = {
