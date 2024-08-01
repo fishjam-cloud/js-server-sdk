fd8673b117430a3cc5e8ac4b74b0bfcf15261225
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-08-01 11:56:54 +0200

    optional webhook url type

--- examples/room-manager/src/config.ts
+++ examples/room-manager/src/config.ts
@@ -36,7 +36,7 @@ declare module 'fastify' {
   interface FastifyInstance {
     config: {
       PORT: number;
-      WEBHOOK_URL: string;
+      WEBHOOK_URL?: string;
       PEERLESS_PURGE_TIMEOUT?: number;
       ENABLE_SIMULCAST: boolean;
       MAX_PEERS?: number;
