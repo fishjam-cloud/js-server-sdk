29b55e78181e56a8ac89f5a705f01126ac93fc97
Miłosz Filimowski <MiloszFilimowski@users.noreply.github.com>
2025-02-07 11:38:19 +0100

    FCE-1185: Change default codec for video-room to VP8 (#102)
    ## Description
    
    - Set default codec do vp8 when creating new room with room manager
    
    ## Motivation and Context
    
    - Android emulator doesn't support H264 by default, so this will allow
    for better compatibility
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- packages/js-server-sdk/src/index.ts
+++ packages/js-server-sdk/src/index.ts
@@ -1,4 +1,4 @@
-export { Peer, PeerStatus, RoomConfig, PeerOptions } from '@fishjam-cloud/fishjam-openapi';
+export { Peer, PeerStatus, RoomConfig, PeerOptions, RoomConfigVideoCodecEnum } from '@fishjam-cloud/fishjam-openapi';
 export { FishjamWSNotifier } from './ws_notifier';
 export { FishjamClient } from './client';
 export * from './exceptions';
