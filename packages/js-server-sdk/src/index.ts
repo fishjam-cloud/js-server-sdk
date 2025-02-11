1a532c50c5b4d7926c95bdaecd3024accc23873d
Miron Pawlik <miron.pawlik@swmansion.com>
2025-02-11 10:16:29 +0100

    Update room manager

--- packages/js-server-sdk/src/index.ts
+++ packages/js-server-sdk/src/index.ts
@@ -1,4 +1,4 @@
-export { PeerStatus, RoomConfig, PeerOptions } from '@fishjam-cloud/fishjam-openapi';
+export { PeerStatus, RoomConfig, PeerOptions, RoomConfigVideoCodecEnum } from '@fishjam-cloud/fishjam-openapi';
 export { FishjamWSNotifier } from './ws_notifier';
 export type { CloseEventHandler, ErrorEventHandler, NotificationEvents, ExpectedEvents } from './ws_notifier';
 export { FishjamClient } from './client';
