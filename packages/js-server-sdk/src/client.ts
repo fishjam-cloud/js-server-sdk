7863135b144f6d8c15e112f1a8e87180587919ee
Miron Pawlik <mironiasty@users.noreply.github.com>
2025-02-11 09:39:14 +0100

    [FCE-1127] Update API docs (#100)
    ## Description
    
    It would be nice ot have some TypeDoc comments added to project. This
    way we can add js-server-doc API into our official documentation
    
    ## Motivation and Context
    
    Our server SDKs are not well documented, so when clients will decide to
    implement their backend - it won't be as easy as code for frontend
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- packages/js-server-sdk/src/client.ts
+++ packages/js-server-sdk/src/client.ts
@@ -3,6 +3,11 @@ import { RoomApi, RoomConfig, PeerOptions, Peer } from '@fishjam-cloud/fishjam-o
 import { FishjamConfig, Room } from './types';
 import { raiseExceptions } from './exceptions/mapper';
 
+/**
+ * Client class that allows to manage Rooms and Peers for a Fishjam App.
+ * It requires the Fishjam URL and management token that can be retrieved from the Fishjam Dashboard.
+ * @category Client
+ */
 export class FishjamClient {
   private readonly roomApi: RoomApi;
 
@@ -16,6 +21,45 @@ export class FishjamClient {
     this.roomApi = new RoomApi(undefined, config.fishjamUrl, client);
   }
 
+  /**
+   * Create a new room. All peers connected to the same room will be able to send/receive streams to each other.
+   */
+  async createRoom(config: RoomConfig = {}): Promise<Room> {
+    const response = await this.roomApi.createRoom(config).catch(raiseExceptions);
+
+    const {
+      data: {
+        data: {
+          room: { components: _, ...room },
+        },
+      },
+    } = response;
+
+    return room;
+  }
+
+  /**
+   * Delete an existing room. All peers connected to this room will be disconnected and removed.
+   * @param roomId
+   */
+  async deleteRoom(roomId: string): Promise<void> {
+    await this.roomApi.deleteRoom(roomId).catch((error) => raiseExceptions(error, 'room'));
+  }
+
+  /**
+   * Get a list of all existing rooms.
+   */
+  async getAllRooms(): Promise<Room[]> {
+    const getAllRoomsRepsonse = await this.roomApi.getAllRooms().catch(raiseExceptions);
+    return getAllRoomsRepsonse.data.data.map(({ components: _, ...room }) => room) ?? [];
+  }
+
+  /**
+   * Create a new peer assigned to a room.
+   * @param roomId
+   * @param options
+   * @returns
+   */
   async createPeer(roomId: string, options: PeerOptions = {}): Promise<{ peer: Peer; peerToken: string }> {
     const response = await this.roomApi
       .addPeer(roomId, {
@@ -31,39 +75,33 @@ export class FishjamClient {
     return { peer: data.peer, peerToken: data.token };
   }
 
-  async createRoom(config: RoomConfig = {}): Promise<Room> {
-    const response = await this.roomApi.createRoom(config).catch(raiseExceptions);
-
-    const {
-      data: {
-        data: {
-          room: { components: _, ...room },
-        },
-      },
-    } = response;
-
-    return room;
-  }
-
-  async getAllRooms(): Promise<Room[]> {
-    const getAllRoomsRepsonse = await this.roomApi.getAllRooms().catch(raiseExceptions);
-    return getAllRoomsRepsonse.data.data.map(({ components: _, ...room }) => room) ?? [];
-  }
-
+  /**
+   * Get details about a given room.
+   * @param roomId
+   * @returns
+   */
   async getRoom(roomId: string): Promise<Room> {
     const getRoomResponse = await this.roomApi.getRoom(roomId).catch((error) => raiseExceptions(error, 'room'));
     const { components: _, ...room } = getRoomResponse.data.data;
     return room;
   }
 
+  /**
+   * Delete a peer - this will also disconnect the peer from the room.
+   * @param roomId
+   * @param peerId
+   */
   async deletePeer(roomId: string, peerId: string): Promise<void> {
     await this.roomApi.deletePeer(roomId, peerId).catch((error) => raiseExceptions(error, 'peer'));
   }
 
-  async deleteRoom(roomId: string): Promise<void> {
-    await this.roomApi.deleteRoom(roomId).catch((error) => raiseExceptions(error, 'room'));
-  }
-
+  /**
+   * Refresh the peer token for an already existing peer.
+   * If an already created peer has not been connected to the room for more than 24 hours, the token will become invalid. This method can be used to generate a new peer token for the existing peer.
+   * @param roomId
+   * @param peerId
+   * @returns refreshed peer token
+   */
   async refreshPeerToken(roomId: string, peerId: string): Promise<string> {
     const refreshTokenResponse = await this.roomApi
       .refreshToken(roomId, peerId)
