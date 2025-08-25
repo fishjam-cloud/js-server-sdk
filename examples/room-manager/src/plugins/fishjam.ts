0a40b0ef78d1e9023d4d9d2796e6763e851b0b88
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2025-08-25 16:57:48 +0200

    remove irrelevant fallback

--- examples/room-manager/src/plugins/fishjam.ts
+++ examples/room-manager/src/plugins/fishjam.ts
@@ -37,7 +37,7 @@ export const fishjamPlugin = fastifyPlugin(async (fastify: FastifyInstance): Pro
   const fishjamClient = new FishjamClient({
     fishjamId: fastify.config.FISHJAM_ID,
     fishjamUrl: fastify.config.FISHJAM_URL,
-    managementToken: fastify.config.FISHJAM_MANAGEMENT_TOKEN ?? 'development',
+    managementToken: fastify.config.FISHJAM_MANAGEMENT_TOKEN,
   });
 
   const peerNameToAccessMap = new Map<string, PeerAccessData>();
