faec0bac0b2efda89b2b5bb3e77066a77d70c03c
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2025-08-25 16:44:37 +0200

    remove stale property occurrences

--- examples/room-manager/src/plugins/fishjam.ts
+++ examples/room-manager/src/plugins/fishjam.ts
@@ -37,7 +37,7 @@ export const fishjamPlugin = fastifyPlugin(async (fastify: FastifyInstance): Pro
   const fishjamClient = new FishjamClient({
     fishjamId: fastify.config.FISHJAM_ID,
     fishjamUrl: fastify.config.FISHJAM_URL,
-    managementToken: fastify.config.FISHJAM_MANAGEMENT_TOKEN ?? fastify.config.FISHJAM_SERVER_TOKEN ?? 'development',
+    managementToken: fastify.config.FISHJAM_MANAGEMENT_TOKEN ?? 'development',
   });
 
   const peerNameToAccessMap = new Map<string, PeerAccessData>();
