e95069bde26d2772fd2b14352eac720e98f2fb05
Jakub Perżyło <jakub.perzylo@swmansion.com>
2026-02-09 13:37:11 +0100

    FCE-2759 Add support for agent image capture (#228)
    ## Description
    
    This PR adds support for agent image capture.
    
    ## Motivation and Context
    
    Why is this change required? What problem does it solve? If it fixes an
    open
    issue, please link to the issue here.
    
    ## Documentation impact
    
    - [ ] Documentation update required
    - [ ] Documentation updated [in another PR](_)
    - [ ] No documentation update required
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- packages/js-server-sdk/src/agent.ts
+++ packages/js-server-sdk/src/agent.ts
@@ -7,6 +7,7 @@ import {
   AgentRequest_TrackData,
   AgentResponse,
   AgentResponse_TrackData,
+  AgentResponse_TrackImage,
   Track as ProtoTrack,
   TrackType as ProtoTrackType,
   TrackEncoding,
@@ -14,13 +15,14 @@ import {
 import { AgentCallbacks, Brand, FishjamConfig, PeerId } from './types';
 import { getFishjamUrl, httpToWebsocket, WithPeerId } from './utils';
 
-const expectedEventsList = ['trackData'] as const;
+const expectedEventsList = ['trackData', 'trackImage'] as const;
 /**
  * @useDeclaredType
  */
 export type ExpectedAgentEvents = (typeof expectedEventsList)[number];
 
 export type IncomingTrackData = Omit<NonNullable<AgentResponse_TrackData>, 'peerId'> & { peerId: PeerId };
+export type IncomingTrackImage = NonNullable<AgentResponse_TrackImage>;
 export type OutgoingTrackData = Omit<NonNullable<AgentRequest_TrackData>, 'peerId'> & { peerId: PeerId };
 
 export type AgentTrack = Omit<ProtoTrack, 'id'> & { id: TrackId };
@@ -122,6 +124,15 @@ export class FishjamAgent extends (EventEmitter as new () => TypedEmitter<AgentE
     const trackData = AgentRequest.encode({ trackData: { trackId: trackId, data: data } }).finish();
 
     this.client.send(trackData);
+  }
+
+  /**
+   * Request a captured image from the given track
+   */
+  public captureImage(trackId: TrackId): void {
+    const msg = AgentRequest.encode({ captureImage: { trackId: trackId } }).finish();
+
+    this.client.send(msg);
   }
 
   public disconnect(): void {
