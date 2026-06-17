15f0552fc6313c00538b9c8cb41aee43020f2f5d
Bernard Gawor <gaworbernard@gmail.com>
2026-06-17 17:27:58 +0200

    Remove composition subpath from js-server-sdk

--- packages/js-server-sdk/src/composition/index.ts
+++
@@ -1,8 +0,0 @@
-export { useCompositionEvent } from './useCompositionEvent';
-export { CompositionEventProvider } from './context';
-export {
-  createCompositionEventBus,
-  type CompositionEventBus,
-  type CompositionEventHandler,
-  type CompositionEventSource,
-} from './eventBus';
