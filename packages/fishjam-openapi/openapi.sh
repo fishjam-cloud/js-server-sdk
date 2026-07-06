c39050f34ea083681bbc34fd19aa764ec45e9e17
Bernard Gawor <gaworbernard@gmail.com>
2026-07-06 12:56:44 +0200

    FCE-3523 regenerate fishjam-openapi with typescript-fetch to drop axios

--- packages/fishjam-openapi/openapi.sh
+++ packages/fishjam-openapi/openapi.sh
@@ -1,24 +1,35 @@
 #!/bin/sh
 
 # Usage:
-# sh openapi.sh main
-# sh openapi.sh v0.1.0
+# sh openapi.sh main <github-token>
+# sh openapi.sh v0.1.0 <github-token>
+# sh openapi.sh path/to/openapi.yaml
 
 set -e
 
 ROOTDIR=$(dirname "$(readlink -f $0)")
 
 if [ -z "$1" ]; then
-  echo "Missing a tag or a branch name as the first argument" 1>&2
-  exit 1
-elif [ -z "$2" ]; then
-  echo "Missing github token as as second argument" 1>&2
+  echo "Missing a tag/branch name or a local spec file path as the first argument" 1>&2
   exit 1
 fi
-echo "Generating code for $1...\n"
+
+if [ -f "$1" ]; then
+  SPEC=$(readlink -f "$1")
+  echo "Generating code from local spec $SPEC...\n"
+else
+  if [ -z "$2" ]; then
+    echo "Missing github token as the second argument" 1>&2
+    exit 1
+  fi
+  SPEC="https://raw.githubusercontent.com/fishjam-cloud/fishjam/refs/heads/$1/openapi.yaml?token=$2"
+  echo "Generating code for $1...\n"
+fi
 
 cd $ROOTDIR \
 && npx @openapitools/openapi-generator-cli generate \
-  -i https://raw.githubusercontent.com/fishjam-cloud/fishjam/refs/heads/$1/openapi.yaml?token=$2 \
-  -g typescript-axios \
+  -i "$SPEC" \
+  -g typescript-fetch \
+  --additional-properties=modelPropertyNaming=original \
+  --reserved-words-mappings public=public \
   -o ./src/generated
---
+++ packages/fishjam-openapi/src/generated/apis/index.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CredentialsApi';
+export * from './MoQApi';
+export * from './RoomsApi';
+export * from './StreamersApi';
+export * from './StreamsApi';
+export * from './TrackForwardingsApi';
+export * from './ViewersApi';
